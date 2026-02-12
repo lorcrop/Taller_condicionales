7210e4c45a4f3d7cba69fb119bca23d618d4c7bd
lorcrop <andresperez@colegioguanenta.edu.co>
2026-02-12 16:07:32 -0500

    taller de ejercicios sobre condicionales

---
+++ numero_par/README.md
@@ -0,0 +1,4 @@
+# Ejercicio #6
+- progrma para determinar si un numero es par 
+## Diagrama de flujo 
+![alt text](image.png)
---
+++ numero_par/ejercicio_3.py
@@ -0,0 +1,8 @@
+# Solicitar número
+N= int(input("Ingrese un número: "))
+print("------------------")
+# Verificar si es par
+if N % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
