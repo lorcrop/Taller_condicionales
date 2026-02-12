f91f8efa4ea30bbb2335496a6c7c18418e1fc5ce
lorcrop <andresperez@colegioguanenta.edu.co>
2026-02-12 16:22:23 -0500

    taller de ejercicios sobre condicionales

---
+++ numeros_iguales/README.md
@@ -0,0 +1,4 @@
+# Ejercicio #4
+. programa paradeterminar si los dos ultimos digitos son iguales o diferentes 
+## Diagrama de flujo
+![alt text](image.png)
---
+++ numeros_iguales/ejercicio_4.py
@@ -0,0 +1,16 @@
+# inicio
+n= input("Ingrese un número: ")
+
+# Verificar que tenga al menos dos dígitos
+if len(n) < 2:
+    print("El número debe tener al menos dos dígitos.")
+else:
+    # Obtener los dos últimos dígitos
+    ultimo = n[-1]
+    penultimo = n [-2]
+
+    # Comparar
+    if ultimo == penultimo:
+        print("Los dos últimos dígitos son iguales ")
+    else:
+        print("Los dos últimos dígitos son diferentes ")
